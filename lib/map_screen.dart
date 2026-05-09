3ed7750b7740fd47617eba40f59cb2352ed83775
Oliver-Eriksson <oliver.j.h.eriksson@gmail.com>
2026-05-09 13:19:00 +0200

    added map

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Map Page',
-        style: TextStyle(fontSize: 24),
-      ),
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Map'),
+          centerTitle: true,
+        ),
+        body: SizedBox.expand(
+          child: OSMFlutter(
+            controller: MapController(
+              initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+              areaLimit: const BoundingBox(
+                east: 10.4922941,
+                north: 47.8084648,
+                south: 45.817995,
+                west: 5.9559113,
+              ),
+            ),
+            osmOption: OSMOption(
+              userTrackingOption: const UserTrackingOption(
+                enableTracking: true,
+                unFollowUser: false,
+              ),
+            zoomOption: const ZoomOption(
+              initZoom: 8,
+              minZoomLevel: 3,
+              maxZoomLevel: 19,
+              stepZoom: 1.0,
+            ),
+            userLocationMarker: UserLocationMaker(
+              personMarker: const MarkerIcon(
+                icon: Icon(
+                  Icons.location_history_rounded,
+                  color: Colors.red,
+                  size: 48,
+                ),
+              ),
+              directionArrowMarker: const MarkerIcon(
+                icon: Icon(
+                  Icons.double_arrow,
+                  size: 48,
+                ),
+              ),
+            ),
+            roadConfiguration: const RoadOption(
+              roadColor: Colors.yellowAccent,
+            ),
+          ),
+        )
+      )
     );
   }
 }
